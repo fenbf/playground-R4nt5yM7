b72bc7f655759f7d0b0207faa220b84090b1a206
Bartłomiej Filipek <joebaf@gmail.com>
2017-09-20 14:56:58 +0200

    test

--- cpp/run_with_param.sh
+++ cpp/run_with_param.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 g++ -std=c++1z -O2 -Wall $1 -lstdc++fs
 ./a.out $2 $3 $4 $5
-cmp --silent correct_output.csv $5 || echo "files are different"
+echo comparing "correct_output.csv" with $5
+cmp --silent correct_output.csv $5 || echo "files are different!" 
 #echo "TECHIO> terminal -i ./a.out"
 #sleep 60
