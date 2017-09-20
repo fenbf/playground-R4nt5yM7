b3ce1d0917b34307db90b9b4b0e9ad2b71c4caee
Bartłomiej Filipek <joebaf@gmail.com>
2017-09-20 15:02:43 +0200

    cleanup

--- cpp/cpp_param_test.cpp
+++ cpp/cpp_param_test.cpp
@@ -1,5 +1,21 @@
-// filesystem_path_example.cpp  
-// compile by using: /EHsc  
+/*
+sample code, just to start, it's run with the following commands:
+
+g++ -std=c++1z -O2 -Wall $1 -lstdc++fs
+./a.out $2 $3 $4 $5
+echo comparing "correct_output.csv" with $5
+cmp --silent correct_output.csv $5 || echo "files are different!" 
+
+where 
+  $1 - cpp file name
+  $2 - name of input csv
+  $3 - column name
+  $4 - replacement string
+  $5 - output file name
+
+*/
+
+
 #include <string>  
 #include <iostream>  
 
--- cpp/run_with_param.sh
+++ cpp/run_with_param.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-g++ -std=c++1z -O2 -Wall $1 -lstdc++fs
+g++ -std=c++1z -O2 -Wall -pedantic $1 -lstdc++fs -pthread
 ./a.out $2 $3 $4 $5
 echo comparing "correct_output.csv" with $5
 cmp --silent correct_output.csv $5 || echo "files are different!" 
