1df830759d06b87adfb758834f22c45d8894111a
Bartłomiej Filipek <joebaf@gmail.com>
2018-07-03 14:05:17 +0200

    run checks

--- cpp/run_with_terminal.sh
+++ cpp/run_with_terminal.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 g++ -std=c++1z -O2 -Wall $1
-echo "TECHIO> terminal -i ./a.out"
-sleep 60
+
+if [ -f "a.out" ]
+then
+	echo "TECHIO> terminal -i ./a.out"
+	sleep 60
+else
+	echo "compilation failed"
+fi
